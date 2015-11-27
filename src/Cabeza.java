ecdf4c9b1c6b8784929f73e18104b19c1ef3c9d0
ClsHdz <carlos.hdz97@gmail.com>
2015-11-27 16:27:32 +0100

    Añadido cuello.

--- src/Cabeza.java
+++ src/Cabeza.java
@@ -5,6 +5,7 @@ public class Cabeza extends GraphicsProgram{
 	public void Cabeza(int ancho, int alto, double posX, double posY){
 		GRect cabeza = new GRect(ancho,alto);
 		GRect boca = new GRect(ancho/2, alto/4);
+		GRect cuello = new GRect(ancho/2, alto/8);
 		GOval ojo1 = new GOval(ancho/4, ancho/4);
 		GOval ojo2 = new GOval(ancho/4, ancho/4);
 		
@@ -12,6 +13,7 @@ public class Cabeza extends GraphicsProgram{
 		add(cabeza,posX,posY);
 		add(boca,posX + (ancho/4),posY + ((alto/4) * 3));
 		add(ojo1,posX + (ancho/4),posY + (alto/4));
-		add(ojo2,posX + ((ancho/4) * 3),posY + (alto/4));
+		add(ojo2,posX + (ancho/2),posY + (alto/4));
+		add(cuello,posX + (ancho/4), posY + (alto));
 	}
 }
