0b5acdc0b543cf76eec8940925f94a26191467eb
xfloki <alexdietta@hotmail.com>
2015-11-27 16:28:01 +0100

    Terminadas las piernas del Robot

--- src/PiernaRobot.java
+++ src/PiernaRobot.java
@@ -1,4 +1,41 @@
+import acm.program.*;
+import acm.graphics.*;
 
-public class ClaseNueva {
+public class PiernaRobot extends GraphicsProgram  {
+	
+	private static int ANCHO_PIERNA = 50 ;
+	private static int LARGO_PIERNA = 150 ;
+	
+	private static int ANCHO_PIE = 75 ;
+	private static int ALTO_PIE = 25 ;
+	
+	public void run(){
+		piernas();
+		pies();
+	}
+	
+	public void piernas(){
+
+		GRect piernaIzquierda = new GRect(ANCHO_PIERNA,LARGO_PIERNA);
+		GRect piernaDerecha = new GRect(ANCHO_PIERNA,LARGO_PIERNA);
+		
+		
+		add(piernaIzquierda,100,0);
+		add(piernaDerecha,155,0);
+	}
+	
+	public void pies(){
+		GRect pieIzquierdo = new GRect(ANCHO_PIE,ALTO_PIE);
+		GRect pieDerecho = new GRect(ANCHO_PIE,ALTO_PIE);
+		
+		add(pieIzquierdo, 155 ,150);
+		add(pieDerecho,75,150);
+	}
+	
+	public void legs(){
+		piernas();
+		pies();
+	}
+	
 
 }
